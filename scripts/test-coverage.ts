867a1558af5102631ce44ea2f9f4b6f15691cd69
Michael Naumov <mnaoumov@gmail.com>
2026-03-21 21:53:25 -0600

    chore: enable integration tests

--- scripts/test-coverage.ts
+++ scripts/test-coverage.ts
@@ -1,4 +1,8 @@
 import { wrapCliTask } from 'obsidian-dev-utils/script-utils/cli-utils';
 import { testCoverage } from 'obsidian-dev-utils/script-utils/test-runners/vitest';
 
-await wrapCliTask(() => testCoverage());
+await wrapCliTask(() =>
+  testCoverage({
+    projects: ['unit-tests']
+  })
+);
