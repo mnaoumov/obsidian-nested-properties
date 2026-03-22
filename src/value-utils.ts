1c4110bbdd7b4f9a627e63999b12e9f143da001a
Michael Naumov <mnaoumov@gmail.com>
2026-03-21 19:19:11 -0600

    feat: convert object <-> list

--- src/value-utils.ts
+++ src/value-utils.ts
@@ -1,37 +1,23 @@
 import type { GenericObject } from 'obsidian-dev-utils/type-guards';
 
 export function convertValue(value: unknown, targetType: string): unknown {
-  // eslint-disable-next-line @typescript-eslint/no-base-to-string -- We want to convert the value to a string.
-  const str = String(value ?? '');
   switch (targetType) {
     case 'aliases':
     case 'multitext':
     case 'tags':
-      if (Array.isArray(value)) {
-        return value;
-      }
-      if (str) {
-        return [str];
-      }
-      return [];
+      return convertToList(value);
     case 'checkbox':
       return Boolean(value);
     case 'date':
     case 'datetime':
-      if (typeof value === 'string' && value && window.moment(value).isValid()) {
-        return value;
-      }
-      return null;
+      return convertToDate(value);
     case 'number':
-      return Number(str) || 0;
+      return convertToNumber(value);
     case 'object':
-      if (value !== null && typeof value === 'object') {
-        return value;
-      }
-      return {};
+      return convertToObject(value);
     case 'text':
     default:
-      return str;
+      return convertToString(value);
   }
 }
 
@@ -41,4 +27,44 @@ export function isComplexValue(value: unknown): value is GenericObject | unknown
 
 export function isSimpleArray(value: unknown): boolean {
   return Array.isArray(value) && value.every((item) => !isComplexValue(item));
+}
+
+function convertToDate(value: unknown): null | string {
+  if (typeof value === 'string' && value && window.moment(value).isValid()) {
+    return value;
+  }
+  return null;
+}
+
+function convertToList(value: unknown): unknown[] {
+  if (Array.isArray(value)) {
+    return value;
+  }
+  if (value !== null && typeof value === 'object') {
+    return Object.values(value as GenericObject);
+  }
+  const str = convertToString(value);
+  if (str) {
+    return [str];
+  }
+  return [];
+}
+
+function convertToNumber(value: unknown): number {
+  return Number(convertToString(value)) || 0;
+}
+
+function convertToObject(value: unknown): GenericObject {
+  if (Array.isArray(value)) {
+    return Object.fromEntries(value.map((item, index) => [String(index), item]));
+  }
+  if (value !== null && typeof value === 'object') {
+    return value as GenericObject;
+  }
+  return {};
+}
+
+function convertToString(value: unknown): string {
+  // eslint-disable-next-line @typescript-eslint/no-base-to-string -- We want to convert the value to a string.
+  return String(value ?? '');
 }
